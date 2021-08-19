499ccd4015c74173fae87faf989acd132fe03974
tamas <tjplug144@gmail.com>
2021-08-19 15:44:00 +0100

    adding bgp and routing-policy examples

--- oc-peering.py
+++ oc-peering.py
@@ -1,6 +1,12 @@
 import requests
 import json
 from pprint import pprint as pp
+from openconfig_bgp import openconfig_bgp
+from openconfig_routing_policy import openconfig_routing_policy
+from openconfig_interfaces import openconfig_interfaces
+import pyangbind.lib.pybindJSON as pybindJSON
+import subprocess
+
 url = "https://www.peeringdb.com/api/net?asn=44194&depth=2&="
 
 payload={}
@@ -17,4 +23,44 @@ for i in data['data'][0]['netixlan_set']:
         ipaddr6 = i['ipaddr6']
 print (name)
 print(f"ASN: {asn} \nIPV4: {ipaddr4} \nIPV6:{ipaddr6}")
+
+fullCmd = "bgpq4 -4 -A -j -l temp {}".format(irr_as_set)
+output = subprocess.check_output(fullCmd, shell=True)
+bgpq = json.loads(output)
+
+# prefix-list
+
+oc = openconfig_routing_policy()
+pfxname = str(asn) + 'v4'
+oc.routing_policy.defined_sets.prefix_sets.prefix_set.add(pfxname)
+for pfxlist in bgpq['temp']:
+    oc.routing_policy.defined_sets.prefix_sets.prefix_set[pfxname].prefixes.prefix.add(ip_prefix=pfxlist['prefix'], masklength_range='exact')
+
+with open("prefixlists.json", "w") as f:
+    f.write(pybindJSON.dumps(oc.routing_policy, mode="ietf"))
+
+# BGP section
+
+oc = openconfig_bgp()
+
+oc.bgp.neighbors.neighbor.add(ipaddr4)
+oc.bgp.neighbors.neighbor[ipaddr4].config.neighbor_address=ipaddr4
+oc.bgp.neighbors.neighbor[ipaddr4].config.enabled=True
+oc.bgp.neighbors.neighbor[ipaddr4].config.peer_as=asn
+oc.bgp.neighbors.neighbor[ipaddr4].config.description=name
+
+oc.bgp.neighbors.neighbor.add(ipaddr6)
+oc.bgp.neighbors.neighbor[ipaddr6].config.neighbor_address=ipaddr6
+oc.bgp.neighbors.neighbor[ipaddr6].config.enabled=True
+oc.bgp.neighbors.neighbor[ipaddr6].config.peer_as=asn
+oc.bgp.neighbors.neighbor[ipaddr6].config.description=name
+with open("bgp.json", "w") as f:
+    f.write(pybindJSON.dumps(oc.bgp, mode="ietf"))
+
+# interfaces
+
+
+# with open("interfaces.json", "w") as f:
+#     f.write(pybindJSON.dumps(oc.bgp, mode="ietf"))
+
 
