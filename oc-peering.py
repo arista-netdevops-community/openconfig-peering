59e5742754cb3d1002b9032256a90b4ebad77dcc
tamas <tjplug144@gmail.com>
2021-08-19 16:21:47 +0100

    add ip/prefix example

--- oc-peering.py
+++ oc-peering.py
@@ -3,7 +3,7 @@ import json
 from pprint import pprint as pp
 from openconfig_bgp import openconfig_bgp
 from openconfig_routing_policy import openconfig_routing_policy
-from openconfig_interfaces import openconfig_interfaces
+from openconfig_interfaces_merged import openconfig_interfaces
 import pyangbind.lib.pybindJSON as pybindJSON
 import subprocess
 
@@ -64,10 +64,12 @@ oc.interfaces.interface.add('Ethernet1')
 oc.interfaces.interface['Ethernet1'].config.description = 'IXP Port'
 oc.interfaces.interface['Ethernet1'].config.enabled = True
 ip = "193.178.185.250"
-prefix_length= "24"
+prefix_length = 24
 oc.interfaces.interface['Ethernet1'].subinterfaces.subinterface.add(0)
-#oc.interfaces.interface['Ethernet1'].subinterfaces.subinterface[0].ipv4.addresses.add(ip=ip,prefix_length=prefix_length)
-oc.interfaces.interface['Ethernet1'].subinterfaces.subinterface[0].ipv4.addresses.config.ip=ip
+oc.interfaces.interface['Ethernet1'].subinterfaces.subinterface[0].ipv4.addresses.address.add(ip=ip)
+oc.interfaces.interface['Ethernet1'].subinterfaces.subinterface[0].ipv4.addresses.address[ip].config.ip = ip
+oc.interfaces.interface['Ethernet1'].subinterfaces.subinterface[0].ipv4.addresses.address[ip].config.prefix_length = 24
+#oc.interfaces.interface['Ethernet1'].subinterfaces.subinterface[0].ipv4.addresses.config.ip=ip
 
 with open("interfaces.json", "w") as f:
     f.write(pybindJSON.dumps(oc.interfaces, mode="ietf"))
