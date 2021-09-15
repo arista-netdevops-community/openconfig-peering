dba438e9eabbf15cd48ff7cd89e2b7f27c16ac5f
Florian Hibler <florian@arista.com>
2021-09-15 18:24:08 +0200

    fix typo on routing_policy script

--- scripts/gnmi_routing_policy.sh
+++ scripts/gnmi_routing_policy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-../tools/gnmic/gnmic -a 192.168.3.3:6030 -u openconfig -p pass openconfig --insecure  \
+../tools/gnmic/gnmic -a 192.168.3.3:6030 -u openconfig -p openconfig --insecure set \
 --update-path '/routing-policy/' \
 --update-file ../json/routing_policy.json
